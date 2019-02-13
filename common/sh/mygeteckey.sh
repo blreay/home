9e8a357654d3783f89d67f11cc20d0b40b35f87b
楚一 <zhaoyong.zzy@antfin.com>
2019-02-13 17:48:40 +0800

    enh

--- common/sh/mygeteckey.sh
+++ common/sh/mygeteckey.sh
@@ -5,9 +5,9 @@ password=${2?"please input password"}
 
 pemfile=${keyfile}.pem 
 #openssl ec -in ${keyfile} -passin pass:${password} -passout pass:${password} -aes256 -out ${keyfile}.pem
-openssl ec -in ${keyfile} -passin pass:${password} -out ${keyfile}.pem
+openssl ec -in ${keyfile} -passin pass:${password} -out ${pemfile}
 echo "Key:"
-openssl ec -in Tester001.key.pem -inform PEM -text 2>/dev/null | awk '
+openssl ec -in ${pemfile} -inform PEM -text 2>/dev/null | awk '
 	begin{start=0};
 	/priv:/{start=1;next;}; 
 	/pub:/{start=0};
