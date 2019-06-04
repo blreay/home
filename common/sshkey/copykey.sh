5de937ffd80f1a766104bd4f65fefc942898735f
楚一 <zhaoyong.zhang@antfin.com>
2019-06-04 16:47:50 +0800

    encrypt my key

--- common/sshkey/copykey.sh
+++ common/sshkey/copykey.sh
@@ -5,9 +5,16 @@
 mkdir -p $HOME/.ssh
 
 echo "Copy key files"
-cp zzy01_rsa_private $HOME/.ssh/id_rsa
 cp zzy01_rsa_pub.txt $HOME/.ssh/id_rsa.pub
 cat zzy01_rsa_pub.txt >> $HOME/.ssh/authorized_keys
+#cp zzy01_rsa_private $HOME/.ssh/id_rsa 
+#Bxxxxxxx@xxx
+cat - <<EOF > $HOME/.ssh/id_rsa
+`gpg -o - zzy01_rsa_private.gpg 2>/dev/null`
+EOF
+cat - <<EOF > zzy01-private.ppk
+`gpg -o - zzy01-private.ppk.gpg 2>/dev/null`
+EOF
 
 chmod 600 $HOME/.ssh/id_rsa*
 chmod 600 $HOME/.ssh/authorized_keys
