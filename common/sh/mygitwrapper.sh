7698d2b7ff8510c16ff439973df37d847d3a51f2
楚一 <zhaoyong.zhang@antfin.com>
2019-06-14 20:34:07 +0800

    add SSH_PORT

--- common/sh/mygitwrapper.sh
+++ common/sh/mygitwrapper.sh
@@ -4,4 +4,5 @@
 
 #echo $@ > /tmp/zzy
 #ssh -i /u01/app/oracle/tools/home/oracle/home/common/sshkey/zzy01_rsa_private $@
-ssh  -o "StrictHostKeyChecking=no" -i ${NFS}/common/sshkey/zzy01_rsa_private $@
+#SSH_PORT is used to ssh tunnel
+ssh ${SSH_PORT:+-p ${SSH_PORT}} -o "StrictHostKeyChecking=no" -i ${NFS}/common/sshkey/zzy01_rsa_private $@
