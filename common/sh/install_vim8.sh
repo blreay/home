4fae05a128c061c881bab0668a8eb0e6ff108b98
blreay <happyblreay@hotmail.com>
2019-06-18 12:01:42 +0800

    add script to install vim8 on centos or RHEL

---
+++ common/sh/install_vim8.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rpm -Uvh http://mirror.ghettoforge.org/distributions/gf/gf-release-latest.gf.el7.noarch.rpm
+rpm --import http://mirror.ghettoforge.org/distributions/gf/RPM-GPG-KEY-gf.el7
+
+## don't remove them, because sudo will be removed also, just upgrade them is ok
+#yum -y remove vim-minimal vim-common vim-enhanced sudo
+#yum -y --enablerepo=gf-plus install vim-enhanced sudo
+
+yum -y --enablerepo=gf-plus install vim-enhanced vim-minimal
+
