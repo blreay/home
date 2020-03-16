05735cc8fc52da4023bc1935957e8cdb523be059
楚一 <zhaoyong.zzy@antfin.com>
2020-03-16 12:15:16 +0800

    add test case for alias in framework

--- common/sh/mytest.sh
+++ common/sh/mytest.sh
@@ -15,34 +15,79 @@ function runcase {
 	act="$2"
 	${cmd} >/dev/null 2>&1; BCS_CHK_ACT_RC0 "${act}" 2>&1
 }
+function run_and_assert {
+	cmd="$1"
+	act="$2"
+	BCS_ASSERT "${cmd} ${act}"
+    echo "000"
+}
 function run_and_chk {
 	cmd="$1"
 	act="$2"
 	BCS_RUN_AND_CHK "${cmd} ${act}"
 }
-function my_test_BCS_ASSERT {
-    echo "OK"
-}
 
-function my_test_BCS_RUN_AND_CHK {
-    echo "OK"
-    a=8
+function my_test_BCS_ASSERT {
     CMDOK="ls / > /dev/null"
     CMDNG="rm /aaa/ccc 2>/dev/null"
-    ret=$(run_and_chk "$CMDOK")
-    [[ $? -eq 0 && $(echo ${ret}) == "" ]] && echo "ok" || echo "ng ${ret}"
-    ret=$(run_and_chk "$CMDOK" "@@@ failed to run")
-    [[ $? -eq 0 && $(echo ${ret}) == "" ]] && echo "ok" || echo "ng ${ret}"
+    echo "------ CMD OK ------"
+    ret=$(run_and_assert "${CMDOK}")
+    [[ $? -eq 0 && $(echo ${ret}) == "000" ]] && echo "ok" || echo "ng ${ret}"
+    ret=$(run_and_assert "${CMDOK}" "@@@ failed to run")
+    [[ $? -eq 0 && $(echo ${ret}) == "000" ]] && echo "ok" || echo "ng ${ret}"
+    ret=$(run_and_assert "${CMDOK}" "@@@ AAA BBB ${CMD} &&& echo 999")
+    [[ $? -eq 0 && $(echo ${ret}) == "000" ]] && echo "ok" || echo "ng ${ret}"
+    ret=$(run_and_assert "${CMDOK}" "@@@ AAA BBB ${CMD} &&& echo 999 ||| echo 888")
+    [[ $? -eq 0 && $(echo ${ret}) == "888 000" ]] && echo "ok" || echo "ng ${ret}"
+    ret=$(run_and_assert "${CMDOK}" "@@@ AAA BBB ${CMD} &&& echo 999 !!! echo 888")
+    [[ $? -eq 0 && $(echo ${ret}) == "888 000" ]] && echo "ok" || echo "ng ${ret}"
+    ret=$(run_and_assert "${CMDOK}" "@@@ AAA BBB ${CMD} &&& echo 999 ||| echo 888 !!! echo 777")
+    [[ $? -eq 0 && $(echo ${ret}) == "888 777 000" ]] && echo "ok" || echo "ng ${ret}"
 
+    echo "------ CMD NG ------"
+    ret=$(run_and_assert "$CMDNG" 2>&1)
+    [[ $? -ne 0 && $(echo ${ret}) =~ ERROR ]] && echo "ok" || echo "ng ${ret}"
+    ret=$(run_and_assert "$CMDNG" "@@@ AAA BBB" 2>&1)
+    [[ $? -ne 0 && $(echo ${ret}) =~ AAA\ BBB.*RET=1$ ]] && echo "ok" || echo "ng ${ret}"
+    ret=$(run_and_assert "${CMDNG}" "@@@ AAA BBB &&& echo 999" 2>&1)
+    [[ $? -ne 0 && $(echo ${ret}) =~ ^999.*AAA\ BBB.*RET=1$ ]] && echo "ok" || echo "ng ${ret}"
+    ret=$(run_and_assert "${CMDNG}" "@@@ AAA BBB ${CMD} &&& echo 999 ||| echo 888" 2>&1)
+    [[ $? -ne 0 && $(echo ${ret}) =~ ^999.*AAA\ BBB.*RET=1$ ]] && echo "ok" || echo "ng ${ret}"
+    ret=$(run_and_assert "${CMDNG}" "@@@ AAA BBB ${CMD} &&& echo 999 !!! echo 888" 2>&1)
+    [[ $? -ne 0 && $(echo ${ret}) =~ ^999.*888.*AAA\ BBB.*RET=1$ ]] && echo "ok" || echo "ng ${ret}"
+    ret=$(run_and_assert "${CMDNG}" "@@@ AAA BBB ${CMD} &&& echo 999 ||| echo 888 !!! echo 777" 2>&1)
+    [[ $? -ne 0 && $(echo ${ret}) =~ ^999.*777.*AAA\ BBB.*RET=1$ ]] && echo "ok" || echo "ng ${ret}"
+}
+function my_test_BCS_RUN_AND_CHK {
+    CMDOK="ls / > /dev/null"
+    CMDNG="rm /aaa/ccc 2>/dev/null"
+    echo "------ CMD OK ------"
+    ret=$(run_and_chk "${CMDOK}")
+    [[ $? -eq 0 && $(echo ${ret}) == "" ]] && echo "ok" || echo "ng ${ret}"
+    ret=$(run_and_chk "${CMDOK}" "@@@ failed to run")
+    [[ $? -eq 0 && $(echo ${ret}) == "" ]] && echo "ok" || echo "ng ${ret}"
+    ret=$(run_and_chk "${CMDOK}" "@@@ AAA BBB ${CMD} &&& echo 999")
+    [[ $? -eq 0 && $(echo ${ret}) == "" ]] && echo "ok" || echo "ng ${ret}"
+    ret=$(run_and_chk "${CMDOK}" "@@@ AAA BBB ${CMD} &&& echo 999 ||| echo 888")
+    [[ $? -eq 0 && $(echo ${ret}) == "888" ]] && echo "ok" || echo "ng ${ret}"
+    ret=$(run_and_chk "${CMDOK}" "@@@ AAA BBB ${CMD} &&& echo 999 !!! echo 888")
+    [[ $? -eq 0 && $(echo ${ret}) == "888" ]] && echo "ok" || echo "ng ${ret}"
+    ret=$(run_and_chk "${CMDOK}" "@@@ AAA BBB ${CMD} &&& echo 999 ||| echo 888 !!! echo 777")
+    [[ $? -eq 0 && $(echo ${ret}) == "888 777" ]] && echo "ok" || echo "ng ${ret}"
+
+    echo "------ CMD NG ------"
     ret=$(run_and_chk "$CMDNG" 2>&1)
     [[ $? -ne 0 && $(echo ${ret}) =~ ERROR ]] && echo "ok" || echo "ng ${ret}"
     ret=$(run_and_chk "$CMDNG" "@@@ AAA BBB" 2>&1)
-    [[ $? -ne 0 && $(echo ${ret}) == "AAA BBB" ]] && echo "ok" || echo "ng ${ret}"
-
-    echo a
-    BCS_RUN_AND_CHK "${CMD} @@@ failed to run"
-    BCS_RUN_AND_CHK "${CMD}"
-    BCS_RUN_AND_CHK "${CMD} @@@ faild to run ${CMD} &&& echo 999 ||| echo 888 !!! echo 777"
+    [[ $? -ne 0 && $(echo ${ret}) =~ AAA\ BBB.*RET=1$ ]] && echo "ok" || echo "ng ${ret}"
+    ret=$(run_and_chk "${CMDNG}" "@@@ AAA BBB &&& echo 999" 2>&1)
+    [[ $? -ne 0 && $(echo ${ret}) =~ ^999.*AAA\ BBB.*RET=1$ ]] && echo "ok" || echo "ng ${ret}"
+    ret=$(run_and_chk "${CMDNG}" "@@@ AAA BBB ${CMD} &&& echo 999 ||| echo 888" 2>&1)
+    [[ $? -ne 0 && $(echo ${ret}) =~ ^999.*AAA\ BBB.*RET=1$ ]] && echo "ok" || echo "ng ${ret}"
+    ret=$(run_and_chk "${CMDNG}" "@@@ AAA BBB ${CMD} &&& echo 999 !!! echo 888" 2>&1)
+    [[ $? -ne 0 && $(echo ${ret}) =~ ^999.*888.*AAA\ BBB.*RET=1$ ]] && echo "ok" || echo "ng ${ret}"
+    ret=$(run_and_chk "${CMDNG}" "@@@ AAA BBB ${CMD} &&& echo 999 ||| echo 888 !!! echo 777" 2>&1)
+    [[ $? -ne 0 && $(echo ${ret}) =~ ^999.*777.*AAA\ BBB.*RET=1$ ]] && echo "ok" || echo "ng ${ret}"
 }
 function my_entry_BCS_ASSERT {
     BCS_ASSERT "rm /aaa/bbb @@@ faild to delete file /aaa/bbb &&& echo 999 ||| echo 888 !!! echo 777"
