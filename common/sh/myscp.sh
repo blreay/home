056c5fc7ed931ad1d5ef1806d19b9c951ebe2307
楚一 <zhaoyong.zzy@antfin.com>
2020-03-24 15:09:21 +0800

    enh myscp.sh

--- common/sh/myscp.sh
+++ common/sh/myscp.sh
@@ -1,15 +1,15 @@
-#!/bin/ksh
+#!/bin/bash
 
-if [[ -z ${MYPWD} && ! -f ~/.ssh/id_rsa ]]; then
-	echo "Please export MYPWD(your password on object machine) at first"
-	exit 1
-fi
+#if [[ -z ${MYPWD} && ! -f ~/.ssh/id_rsa ]]; then
+	#echo "Please export MYPWD(your password on object machine) at first"
+	#exit 1
+#fi
 
 sshopt="-o ServerAliveCountMax=2 -o ServerAliveInterval=120 -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa"
 
 cmd="${@}"
 #set -vx
-case "$MYPSD" in
+case "${MYPWD}" in
 	"") CMD="scp ${sshopt} ${cmd}" ;;
 	*) CMD="remote-exec.sh \"scp ${cmd}\" $MYPWD" ;;
 esac
