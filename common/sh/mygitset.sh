a05bdba3d170ed34a32f05fc76fe718289302caa
blreay <happyblreay@hotmail.com>
2020-05-14 12:22:21 +0800

    enh script to set diffmerge as confict resolve tool

--- common/sh/mygitset.sh
+++ common/sh/mygitset.sh
@@ -7,12 +7,18 @@ git config --global alias.br branch
 git config --global receive.denyCurrentBranch ignore
 
 if [[ "$(uname)" == "Linux" ]]; then
+	#seems don't need this wrapper
+	#git config --global diff.tool diffmerge
+	#git config --global difftool.prompt true
+	#git config --global difftool.diffmerge.cmd 'mygit_diffmerge_wrapper.sh diff $LOCAL $REMOTE'
+	#git config --global merge.tool diffmerge
+	#git config --global mergetool.prompt true
+	#git config --global mergetool.diffmerge.cmd 'mygit_diffmerge_wrapper.sh merge $LOCAL $REMOTE $BASE $MERGED'
 	git config --global diff.tool diffmerge
-	git config --global difftool.prompt true
-	git config --global difftool.diffmerge.cmd 'mygit_diffmerge_wrapper.sh diff $LOCAL $REMOTE'
+	git config --global difftool.diffmerge.cmd "diffmerge \"\$LOCAL\" \"\$REMOTE\""
 	git config --global merge.tool diffmerge
-	git config --global mergetool.prompt true
-	git config --global mergetool.diffmerge.cmd 'mygit_diffmerge_wrapper.sh merge $LOCAL $REMOTE $BASE $MERGED'
+	git config --global mergetool.diffmerge.trustExitCode true
+	git config --global mergetool.diffmerge.cmd "diffmerge --merge --result=\"\$MERGED\"  \"\$LOCAL\" \"\$BASE\" \"\$REMOTE\""
 else
     git config --global diff.tool bc4
     git config --global difftool.prompt true
