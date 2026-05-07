a4e90ec82b14e9a55df200b707fd4e07559303cf
blreay <happyblreay@hotmail.com>
2026-05-07 15:52:46 +0800

    enh mylink.sh to support preview markdown in bashttpd

--- common/sh/mylink.sh
+++ common/sh/mylink.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 #
+#set -vx
 
+typeset view_mode=0
+while getopts v ch; do
+  case $ch in
+    v) view_mode=1;;
+  esac
+done
+shift $((OPTIND-1))
 typeset file=$1
+
 typeset ip=$(ip route get 1.1.1.1 | sed -n 's/.*src \([0-9.]\+\).*/\1/p')
 typeset prefix="http://${ip}:38080/os_root"
+typeset prefix_view="http://${ip}:38080/myhome/git/bashttpd/md_viewer.html?file="
 
 [[ ${file} =~ ^/ ]] && fullpath=$file || fullpath="$(pwd)/${file}"
-echo "${prefix}/${fullpath}" | myyank.sh
-echo "${prefix}/${fullpath}"
+typeset url="${prefix}/${fullpath}"
+[[ ${view_mode} -eq 1 ]] && url="${prefix_view}/os_root/${fullpath}"
+
+echo "${url}" | myyank.sh
+echo "${url}"
 echo "has been copied to clipboard"
