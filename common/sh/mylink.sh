1ad1da0de93f5e85d3988976e8dc4baedd1e8183
blreay <happyblreay@hotmail.com>
2026-05-15 01:22:39 +0800

    enh mylink.sh: add central markdown files management

--- common/sh/mylink.sh
+++ common/sh/mylink.sh
@@ -4,6 +4,7 @@
 
 typeset view_mode=0
 typeset send_mode=0
+typeset user=zhaoyong.zzy
 while getopts vs ch; do
   case $ch in
     v) view_mode=1;;
@@ -23,10 +24,31 @@ if [[ $send_mode -eq 0 ]]; then
 	[[ ${view_mode} -eq 1 ]] && url="${prefix_view}/os_root/${fullpath}"
 else
 	typeset ip=$MYVM
-	scp $file zhaoyong.zzy@$ip:~/docs/
-	fullpath=/home/zhaoyong.zzy/docs/${file##*/}
-	#typeset url="http://${MYVM}:38080/myhome/git/bashttpd/md_viewer.html?file=${remote_path}"
-	#[[ ${view_mode} -eq 1 ]] && url="${prefix_view}/os_root/${fullpath}"
+	scp $file ${user}@$ip:~/docs/
+	fullpath=/home/${user}/docs/${file##*/}
+
+	# Register to md_index.md on MYVM
+	typeset md_title=$(grep -m1 '^# ' "$file" | sed 's/^# //')
+	[[ -z "$md_title" ]] && md_title="${file##*/}"
+	typeset preview_url="http://${ip}:38080/myhome/git/bashttpd/md_viewer.html?file=/os_root${fullpath}"
+	typeset raw_url="http://${ip}:38080/os_root${fullpath}"
+	typeset ts=$(date '+%Y%m%d_%H%M%S')
+	typeset src_ip=$(ip route get 1.1.1.1 | sed -n 's/.*src \([0-9.]\+\).*/\1/p')
+	typeset src_dir=$(pwd)
+	typeset git_branch=$(git rev-parse --abbrev-ref HEAD 2>/dev/null || echo "N/A")
+	typeset new_row="| [${md_title}](${preview_url}) | [原始md文件](${raw_url}) | ${ts} | ${src_ip} | ${src_dir} | ${git_branch} |"
+
+	typeset index_file='~/docs/md_index.md'
+	typeset header='| 标题 | 原始文件 | 创建时间 | 来源机器 | 来源目录 | Git分支 |'
+	typeset separator='|------|----------|----------|----------|----------|---------|'
+
+	ssh ${user}@$ip "
+		if [[ ! -f ${index_file} ]]; then
+			echo '${header}' > ${index_file}
+			echo '${separator}' >> ${index_file}
+		fi
+		sed -i '2a\\${new_row}' ${index_file}
+	"
 fi
 
 typeset prefix="http://${ip}:38080/os_root"
